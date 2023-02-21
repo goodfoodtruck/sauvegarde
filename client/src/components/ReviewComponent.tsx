4beb3c7293332c4f5622fb8f63bbcde51d0ceb08
goodfoodtruck <mielatomique@protonmail.com>
2023-02-21 11:25:05 +0100

    refactor(client/review): fetch username instead of user

--- client/src/components/ReviewComponent.tsx
+++ client/src/components/ReviewComponent.tsx
@@ -1,16 +1,15 @@
 import { FunctionComponent } from "react"
 import { useApi } from "../hooks/useApi"
 import { Review } from "../interfaces/review"
-import { User } from "../interfaces/user"
 
 export const ReviewComponent: FunctionComponent<Review> = (props: Review) => {
-    const body = JSON.stringify({id: props.userId});
-    const response = useApi(`${import.meta.env.VITE_API_URL}/user/id`, 'POST', body);
-    const user: User = response?.data;
+    const userId = JSON.stringify({id: props.userId});
+    const userResponse = useApi(`${import.meta.env.VITE_API_URL}/user/name`, 'POST', userId);
+    const userName: string = userResponse?.data;
 
     return (
         <>
-            <p>{user ? user.name : "Not found"}</p>
+            <p>{userName ? userName : "Not found"}</p>
             <p>{props.description}</p>
         </>
     )
